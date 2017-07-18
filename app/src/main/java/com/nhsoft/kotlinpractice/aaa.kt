d2ed2c583cb6b98d3710bbf6e595174d77e3ac7a
zhongHaoJie <nhsoft.zhj@hotmail.com>
2017-07-18 14:11:16 +0800

    完成第二部分练习

--- app/src/main/java/com/nhsoft/kotlinpractice/aaa.kt
+++
@@ -1,5 +0,0 @@
-package com.nhsoft.kotlinpractice
-
-/**
- * Created by zhonghaojie on 2017-07-12.
- */
--- kotlin-koans-master/src/ii_collections/_23_CompoundTasks.kt
+++ kotlin-koans-master/src/ii_collections/_23_CompoundTasks.kt
@@ -2,17 +2,20 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return customers.filter { it.orderedProducts.contains(product) }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    val(delivered,undelivered)= orders.partition { it.isDelivered }
+    return delivered.flatMap { it.products }.maxBy { it.price }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return customers.flatMap { it.orders }.flatMap { it.products }.sumBy {
+        if(it.name==product.name) 1 else 0
+    }
 }
